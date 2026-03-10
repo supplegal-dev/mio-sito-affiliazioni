20c04aa297afd92ed0b924a1435ee06127c684ef
tizz00 <tizianaparma2000@gmail.com>
2026-03-10 21:37:53 +0100

    Aggiunto sistema di pubblicazione automatica

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  // Questo serve per far capire ad Astro dove si trova il sito online
   site: 'https://supplegal-dev.github.io',
-  // Questo è fondamentale perché il sito è dentro una sottocartella /mio-sito-affiliazioni/
   base: '/mio-sito-affiliazioni',
   integrations: [tailwind()],
 });
